96e8eff398736eb63f6b594c4abc739eb2514a80
Joel Body <joel.body@proton.me>
2025-07-05 14:34:25 +0100

    Adjusting reverse dns to return list of domains found.

--- src/main.rs
+++ src/main.rs
@@ -5,38 +5,39 @@ use std::fs;
 fn main() {
     let contents = fs::read_to_string("collected.txt").expect("Error reading file");
     let recorded_ips: Vec<&str> = contents.lines().collect();
-    reverse_dns(recorded_ips);
-    let domain= "143-42-1-123.ip.linodeusercontent.com";
-    println!("{}", shrink_domain(domain))
-
+    let domains_found = reverse_dns(recorded_ips);
+    println!("{:?}", domains_found);
 }
-fn reverse_dns(recorded_ips: Vec<&str>) {
+fn reverse_dns(recorded_ips: Vec<&str>) -> Vec<String> {
+    let mut domains: Vec<String> = Vec::new();
     for items in recorded_ips {
         match items.parse::<std::net::IpAddr>() {
             Ok(ip) => match lookup_addr(&ip) {
                 Ok(host) => {
                     let shrunk_host = shrink_domain(host.as_str());
-                    println!("{}", shrunk_host);
+                    domains.push(shrunk_host.clone());
+
 
                 },
-                Err(_) => print!(""),
+                Err(_) => (),
             },
-            Err(_) => print!(""),
+            Err(_) => (),
         }
-    }
+    };
+    domains
 }
 
-fn shrink_domain(domain: &str) -> String {
+fn shrink_domain(domain: &str) -> String{
     let parts: Vec<&str> = domain.split(".").collect();
 
     if parts.len() >= 3 {
-        let output =  (&parts[parts.len()- 3..]).join(".");
-        return output
+        let output = (&parts[parts.len() - 3..]).join(".");
+        output
     } else if parts.len() == 2 {
-        let output =  (&parts[parts.len()- 2..]).join(".");
-        return output
+        let output = (&parts[parts.len() - 2..]).join(".");
+        output
     } else {
-        let output =  (&parts[parts.len()- 2..]).join(".");
-        return output
+        let output = (&parts[parts.len() - 2..]).join(".");
+        output
     }
 }
